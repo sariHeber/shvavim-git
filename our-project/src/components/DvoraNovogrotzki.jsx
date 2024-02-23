d605b12196d9e87b9b0e07f49d0cea5a770e15b7
Dvora <d3254766@gmail.com>
2024-02-23 09:49:57 +0200

    commit 1

---
+++ our-project/src/components/DvoraNovogrotzki.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const DvoraNovogrotzki = () => {
+    return (
+        <div style={{ backgroundColor: 'green', borderWidth: '3px', borderStyle: 'solid' }}>
+            <p>name: Dvora Novogrotzki</p>
+            <p> age: 19</p>
+            <p> phone number: 0583254766</p>
+            <p> email: d3254766@gmail.com</p>
+        </div>
+    )
+}
--- our-project/src/components/ourComponents.jsx
+++ our-project/src/components/ourComponents.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { SariHeimannComp } from "./sariHeimann";
+import { DvoraNovogrotzki } from "./DvoraNovogrotzki";
 
 export const OurComponents = () => {
 
@@ -7,6 +8,7 @@ export const OurComponents = () => {
         <div>
             <h1>our project</h1>
             <SariHeimannComp></SariHeimannComp>
+            <DvoraNovogrotzki></DvoraNovogrotzki>
         </div>
     )
 }
