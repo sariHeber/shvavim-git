d2a9a79ebe8ff1d64bef1379b75520a4a4e704cd
elisheva <e0504116785@gmail.com>
2024-02-23 09:56:12 +0200

    elisheva componnent

---
+++ our-project/src/components/elisheva.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const ElishevaComponnent = () => {
+
+    return (
+        <div style={{ borderColor: 'yellow', borderWidth: '5px', borderStyle: 'dotted' }}>
+            <p> name: elisheva</p>
+            <p> age: 19</p>
+            <p> phone number: 050416785</p>
+            <p> email: e0504116785@gmail.com</p>
+        </div>
+    )
+}
--- our-project/src/components/sariHeimann.jsx
+++ our-project/src/components/sariHeimann.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export const SariHeimannComp = () => {
 
-    return(
-       <div style={{borderColor:'blue', borderWidth:'5px', borderStyle:'dotted'}}>
-        <p>name: sari</p>
-        <p> age: 22</p>
-        <p> phone number: 0548503880</p>
-        <p> email: sa0548503880@gmail.com</p>
-       </div>
+    return (
+        <div style={{ borderColor: 'blue', borderWidth: '5px', borderStyle: 'dotted' }}>
+            <p>name: sari</p>
+            <p> age: 22</p>
+            <p> phone number: 0548503880</p>
+            <p> email: sa0548503880@gmail.com</p>
+        </div>
     )
 }
