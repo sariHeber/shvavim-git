c61cb3cec3e191133e38107e9cc5600180941240
Rivka <rbn9574@gmail.com>
2024-02-23 09:56:41 +0200

    add rivka component

--- our-project/src/components/ourComponents.jsx
+++ our-project/src/components/ourComponents.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { SariHeimannComp } from "./sariHeimann";
+import { Rivka } from "./rivka";
 
 export const OurComponents = () => {
 
@@ -7,6 +8,7 @@ export const OurComponents = () => {
         <div>
             <h1>our project</h1>
             <SariHeimannComp></SariHeimannComp>
+            <Rivka></Rivka>
         </div>
     )
 }
