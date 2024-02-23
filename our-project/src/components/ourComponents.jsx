19396f7fe4c8adb5effd1c1f75e3c80288dbd8d3
sariStern <stern0777879962@gmail.com>
2024-02-23 09:48:45 +0200

    Sari Stern first Commit

--- our-project/src/components/ourComponents.jsx
+++ our-project/src/components/ourComponents.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { SariHeimannComp } from "./sariHeimann";
+import { SariStern } from "./SariStern";
 
 export const OurComponents = () => {
 
@@ -7,6 +8,7 @@ export const OurComponents = () => {
         <div>
             <h1>our project</h1>
             <SariHeimannComp></SariHeimannComp>
+            <SariStern></SariStern>
         </div>
     )
 }
