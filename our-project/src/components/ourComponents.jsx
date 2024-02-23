fbc76776333bef209026ee9c1b2fa742b861c045
malkySinowitz <malkysino@gmail.com>
2024-02-23 10:18:02 +0200

    malky

--- our-project/src/components/ourComponents.jsx
+++ our-project/src/components/ourComponents.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { SariHeimannComp } from "./sariHeimann";
+import { Malky } from "./malky";
 
 export const OurComponents = () => {
 
@@ -7,6 +8,7 @@ export const OurComponents = () => {
         <div>
             <h1>our project</h1>
             <SariHeimannComp></SariHeimannComp>
+            <Malky></Malky>
         </div>
     )
 }
