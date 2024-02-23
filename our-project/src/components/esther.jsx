7d023b928b2d41cf2e76023497c4390de8597ce3
esther kalsmithgit config --global user.name esther <esther286641@gmail.com>
2024-02-23 10:24:03 +0200

    addEstherComp

---
+++ our-project/src/components/esther.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Esther = () => {
+
+    return(
+       <div style={{borderColor:'green', borderWidth:'5px', borderStyle:'dotted'}}>
+        <p>name: esther</p>
+        <p> age: 19</p>
+        <p> phone number: 0583286641</p>
+        <p> email: esther286641@gmail.com</p>
+       </div>
+    )
+}
