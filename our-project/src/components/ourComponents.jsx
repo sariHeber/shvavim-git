fa50143a3e35376ea32db354f3e65b881365ccdc
Dvora <sh5372560@gmail.com>
2024-02-23 09:57:14 +0200

    Dvora Shechter commit

--- our-project/src/components/ourComponents.jsx
+++ our-project/src/components/ourComponents.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { SariHeimannComp } from "./sariHeimann";
+import { DvoraShechterComp } from "./DvoraShechter";
 
 export const OurComponents = () => {
 
@@ -7,6 +8,7 @@ export const OurComponents = () => {
         <div>
             <h1>our project</h1>
             <SariHeimannComp></SariHeimannComp>
+            <DvoraShechterComp></DvoraShechterComp>
         </div>
     )
 }
