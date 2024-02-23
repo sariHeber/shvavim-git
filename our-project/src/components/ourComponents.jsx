42b62e8b96c05150b867d2dffbc37892f750b8c4
TalyaNaima <talya.brajtman@gmail.com>
2024-02-23 10:40:22 +0200

    add talya details

--- our-project/src/components/ourComponents.jsx
+++ our-project/src/components/ourComponents.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { SariHeimannComp } from "./sariHeimann";
+import { TalyaBrajtman } from "./talyaBrajtman";
 
 export const OurComponents = () => {
 
@@ -7,7 +8,7 @@ export const OurComponents = () => {
         <div>
             <h1>our project</h1>
             <SariHeimannComp></SariHeimannComp>
-            <talyaBrajtman></talyaBrajtman>
+            <TalyaBrajtman></TalyaBrajtman>
         </div>
     )
 }
---
+++ our-project/src/components/talyaBrajtman.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const TalyaBrajtman = () => {
+    return <>
+        <div style={{ borderColor: 'blue', borderWidth: '5px', borderStyle: 'dotted' }}>
+            <p>name: talya</p>
+            <p> age: 19</p>
+            <p> phone number: 0583288112</p>
+            <p> email: talya.brajtman@gmail.com</p>
+        </div>
+    </>
+
+}
